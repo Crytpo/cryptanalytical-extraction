5b1ab4d3853cd433c0569834ba73983331a0aef2
Peter Lorenz <peter.lorenz@ntu.edu.sg>
2024-09-09 18:53:24 +0800

    add script files

--- scripts/mnist/bash.sh
+++ scripts/mnist/bash.sh
@@ -6,6 +6,12 @@ python models/train_new_models.py --load_json scripts/mnist/dense/8x2_softmax.js
 python models/train_new_models.py --load_json scripts/mnist/dense/64x2_softmax.json
 
 # cnn
+python models/train_new_models.py --load_json scripts/mnist/cnn/8x1_linear.json
+python models/train_new_models.py --load_json scripts/mnist/cnn/64x1_linear.json
+python models/train_new_models.py --load_json scripts/mnist/cnn/8x2_linear.json
+python models/train_new_models.py --load_json scripts/mnist/cnn/64x2_linear.json
+
+
 python models/train_new_models.py --load_json scripts/mnist/cnn/8x1_fc_linear.json
 python models/train_new_models.py --load_json scripts/mnist/cnn/64x1_fc_linear.json
 python models/train_new_models.py --load_json scripts/mnist/cnn/8x2_fc_linear.json
